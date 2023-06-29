7e3c3d94ca1a1653ad0742b1074d4a911728405c
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-06-29 09:20:05 -0300

    Initial commit

---
+++ src/test/java/com/david/place/PlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
