30d9b82b2c63cfe369f4477640d7bff0b88cf5b6
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-06-29 09:44:46 -0300

    Controller

---
+++ src/main/java/com/david/place/web/PlaceController.java
@@ -0,0 +1,29 @@
+package com.david.place.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.david.place.domain.Place;
+import com.david.place.domain.PlaceService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+
+    private PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Mono<Place>> create(Place place) {
+        var createdPlace = placeService.create(place);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+    }
+}
