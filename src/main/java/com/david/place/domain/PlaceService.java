a820f6b0d43200f54c2cb5920687f03427ae9651
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-06-29 09:36:53 -0300

    Service

---
+++ src/main/java/com/david/place/domain/PlaceService.java
@@ -0,0 +1,19 @@
+package com.david.place.domain;
+
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class PlaceService {
+
+    private PlaceRepository placeRepository;
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    public Mono<Place> create(Place place) {
+        return placeRepository.save(place);
+    }
+}
