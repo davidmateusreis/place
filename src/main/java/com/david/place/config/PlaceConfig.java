ed2bd12e88327245d8c59904237c44d767d0c738
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-06-29 10:46:04 -0300

    Auditing
    An audited entity

--- src/main/java/com/david/place/config/PlaceConfig.java
+++ src/main/java/com/david/place/config/PlaceConfig.java
@@ -2,11 +2,13 @@ package com.david.place.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 import com.david.place.domain.PlaceRepository;
 import com.david.place.domain.PlaceService;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
 
     @Bean
